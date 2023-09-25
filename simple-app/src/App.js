1fae3875834d8f20d3f387886dd88a9d45cc6340
lfernandez55 <lfernandez@weber.edu>
2023-09-24 19:52:17 -0600

    Update App.js

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Learn React . . . .Testing Deploy To Netlify. . .change made 9/24
+        Learn React . . . .Testing Deploy To Netlify. . .change made 9/24 at 7:51
       </header>
     </div>
   );
