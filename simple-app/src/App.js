73aa8d3f9794e09dabafa430d95bbd88bde4d270
lfernandez55 <lfernandez@weber.edu>
2023-10-03 19:43:54 -0600

    Making commit. . .

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         Learn React . . . .Testing Deploy To Netlify. . .change made 9/24 at 7:51
+      another made at 10/3
       </header>
     </div>
   );
